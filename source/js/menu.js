a4cd3c05a4f91f52515d7b85b91918452369c4b9
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-04-29 15:53:10 +0500

    Some changes in catalog

--- source/js/menu.js
+++
@@ -1,9 +0,0 @@
-var menuOpener = document.querySelector(".page-header__toggle");
-var pageHeader = document.querySelector(".page-header");
-var mainNav = document.querySelector(".main-nav")
-
-menuOpener.addEventListener('click', function(evt) {
-  menuOpener.classList.toggle('page-header__toggle--opened');
-  pageHeader.classList.toggle('page-header--opened');
-  mainNav.classList.toggle('main-nav--opened');
-});
