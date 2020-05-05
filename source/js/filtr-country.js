859a762602dec04c5687a59c830bbf3d683c46d5
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-05-05 21:04:36 +0500

    Изменён фильтр

--- source/js/filtr-country.js
+++ source/js/filtr-country.js
@@ -1,9 +1,18 @@
 var filtrOpener = document.querySelector(".filtr-country__button");
 var filtrBody = document.querySelector(".filtr-country__body");
+var filtrContainer = document.querySelector(".filtr-country__alphabet-button-container");
 
-filtrBody.classList.remove("filtr-country__body--opened");
+// filtrBody.classList.remove("filtr-country__body--opened");
 
-filtrOpener.addEventListener('click', function(evt) {
-  filtrBody.classList.toggle("filtr-country__body--opened");
-  filtrOpener.classList.toggle("filtr-country__button--opened")
-})
+if (window.screen.width < 768 && window.screen.width > 320) {
+  filtrOpener.addEventListener('click', function(evt) {
+    filtrBody.classList.add("filtr-country__body--opened");
+    filtrOpener.classList.toggle("filtr-country__button--opened");
+  })
+} else if (window.screen.width >= 768) {
+  filtrOpener.addEventListener('click', function(evt) {
+    filtrContainer.classList.toggle("filtr-country__alphabet-button-container--opened");
+    filtrOpener.classList.toggle("filtr-country__button--opened");
+  })
+}
+
