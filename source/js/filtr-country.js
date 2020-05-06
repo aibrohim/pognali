ba51f8e1c185a43b774db8ffdd05ed703d38fba1
Ibrohim7840 <Ibrohim7840@gmail.com>
2020-05-06 15:53:46 +0500

    catalog mobile pixel perfect

--- source/js/filtr-country.js
+++ source/js/filtr-country.js
@@ -2,17 +2,23 @@ var filtrOpener = document.querySelector(".filtr-country__button");
 var filtrBody = document.querySelector(".filtr-country__body");
 var filtrContainer = document.querySelector(".filtr-country__alphabet-button-container");
 
-// filtrBody.classList.remove("filtr-country__body--opened");
+filtrBody.classList.remove("filtr-country__body--opened");
 
-if (window.screen.width < 768 && window.screen.width > 320) {
-  filtrOpener.addEventListener('click', function(evt) {
-    filtrBody.classList.add("filtr-country__body--opened");
+if (window.screen.width < 768) {
+
+  filtrOpener.addEventListener('click', function(da) {
+    filtrBody.classList.toggle("filtr-country__body--opened");
+    filtrContainer.classList.toggle("filtr-country__alphabet-button-container--opened");
     filtrOpener.classList.toggle("filtr-country__button--opened");
+
+    console.log("salom");
   })
-} else if (window.screen.width >= 768) {
+} else if (window.screen.width > 768) {
+  filtrBody.classList.add("filtr-country__body--opened");
   filtrOpener.addEventListener('click', function(evt) {
     filtrContainer.classList.toggle("filtr-country__alphabet-button-container--opened");
     filtrOpener.classList.toggle("filtr-country__button--opened");
+    console.log("nmadr")
   })
 }
 
